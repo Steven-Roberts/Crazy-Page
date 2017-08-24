93454606f45427a9a40f843ffb637340b95b73c7
Steven Roberts <sroberts994@gmail.com>
2017-08-23 22:45:08 -0400

    Check for duplicate release version with promise

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,11 +4,12 @@ import {buildJs, createLintJs, watchJs} from './tasks/js';
 import {buildJson, watchJson} from './tasks/json';
 import {globs, paths, watchOptions} from './tasks/config.js';
 
+import {access} from 'fs';
 import del from 'del';
 import gulp from 'gulp';
 import {join} from 'path';
 import manifest from './src/manifest';
-import {stat} from 'fs';
+import util from 'util';
 import zipper from 'gulp-zip';
 
 const zipFileName = `${manifest.name} ${manifest.version}.zip`;
@@ -20,18 +21,15 @@ const lint = gulp.parallel(createLintCss(), createLintJs());
 const build = gulp.series(clean, lint,
     gulp.parallel(buildCss, buildImg, iconify, buildJs, buildJson));
 
-const checkVersion = (cb) => {
-    stat(join(paths.release, zipFileName), (err) => {
-        if (err) {
-            return cb();
-        }
-
-        return cb(new Error(`There is already a release "${zipFileName}"`));
-    });
-};
+const checkVersion = () => util
+    .promisify(access)(join(paths.release, zipFileName))
+    .then(() => {
+        throw new Error(`There is already a release "${zipFileName}"`);
+    },
+    () => true);
 
 const zip = () => gulp.src(globs.build)
-    .pipe(zipper(`${manifest.name} ${manifest.version}.zip`))
+    .pipe(zipper(zipFileName))
     .pipe(gulp.dest(paths.release));
 
 const release = gulp.series(build, checkVersion, zip);
